ff95e5bdcf36da84080918d6beec6389f2d00d3c
Magnus Egeland <magnus.egeland@gmail.com>
2025-01-22 10:58:05 +0100

    update for profiling

--- Assignment1/sortingAlgorithms/C/insertion_sort.c
+++ Assignment1/sortingAlgorithms/C/insertion_sort.c
@@ -1,28 +1,50 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 void InsertionSort(int arr[], int len_arr) {
-
-    for(int i = 1; i < len_arr;i++) {
+    for (int i = 1; i < len_arr; i++) {
         int key = arr[i];
-        int j = i -1;
+        int j = i - 1;
 
-        while(j >= 0 && key < arr[j]) {
-            arr[j+1] = arr[j];
+        while (j >= 0 && key < arr[j]) {
+            arr[j + 1] = arr[j];
             j -= 1;
         }
         arr[j + 1] = key;
     }
 }
 
-int main() {
-    int arr[] = {3, 6, 8, 10, 1, 2, 1};
-    int len_arr = sizeof(arr) / sizeof(arr[0]);
-    InsertionSort(arr,len_arr);
-
-    for(int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
-        printf("%d, ",arr[i]);
+void test_insertion_sort(int len_arr) {
+    // Generate a random array of length len_arr
+    int arr[len_arr];
+    for (int i = 0; i < len_arr; i++) {
+       arr[i] = rand() % 2001 - 1000;  // Random numbers between 0 and 999
     }
-    printf("\n");
+
+    // Measure the time to sort the array
+    clock_t start_time = clock();
+    InsertionSort(arr, len_arr);
+    clock_t end_time = clock();
+
+    // Calculate the time taken
+    double time_taken = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+
+    // Output the length of the array and the time taken
+    printf("Array length: %d, Time taken: %f seconds\n", len_arr, time_taken);
+}
+
+int main() {
+    // Set a fixed seed for random number generation
+    srand(42);
+
+    // Test for different array lengths
+    int lengths[] = {100, 1000, 5000, 10000, 20000};
+    int num_tests = sizeof(lengths) / sizeof(lengths[0]);
+
+    for (int i = 0; i < num_tests; i++) {
+        test_insertion_sort(lengths[i]);
+    }
 
     return 0;
 }
